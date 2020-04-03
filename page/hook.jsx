c3d3c1c9c1df3724b1402b05b0ec9d9acdd1243e
zhangning <zhangning@frogshealth.com>
2020-04-03 14:07:29 +0800

    update

--- page/hook.jsx
+++ page/hook.jsx
@@ -1,18 +1,77 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+
+// demo 1
+function Counter({initialCount}) {
+  const [count, setCount] = useState(initialCount);
+  return (
+    <p>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </p>
+  );
+}
+
+// demo 2
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+  }
+}
+
+function Counter2() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={ () => dispatch({ type: 'decrement' }) }>-</button>
+      <button onClick={ () => dispatch({type: 'increment' }) }>+</button>
+    </>
+  );
+}
+
 
 export default function Hook() {
+  // state setState
   const [count, setCount] = useState(0);
 
+  // componentDidMount componentDidUpdate
   useEffect(() => {
     document.title = `You clicked ${count} times`;
-  });
+  }, [count]);
+
+  useEffect(() => {
+    function handleStatusChange(status) {
+      setIsOnline(status.isOnline);
+    }
+
+    // add listener
+    // ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+    return () => {
+      // remove listener
+      // ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+    };
+  }), [];
 
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
+      <p>You clicked { count } times</p>
+      <button onClick={ () => setCount(count + 1) }>
+        set title
       </button>
+
+      <Counter initialCount = { 50 } />
+
+      <Counter2 />
+
     </div>
   );
 }
